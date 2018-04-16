5e8899d41ba972880e64e5325007673b8eb1d04d
smithwebtek <brad@smithwebtek.com>
2018-04-16 08:23:25 -0400

    Change API url to point to Heroku

--- src/store/services/genreService.js
+++ src/store/services/genreService.js
@@ -1,4 +1,4 @@
-const API_URL = "https://swt-movies.herokuapp.com/" || "http://127.0.0.1:3001/api"
+const API_URL = "https://swt-movies.herokuapp.com" || "http://127.0.0.1:3001/api"
 // const API_URL = "http://127.0.0.1:3001/api" || "https://swt-movies.herokuapp.com"
 
 const GenreService = {
